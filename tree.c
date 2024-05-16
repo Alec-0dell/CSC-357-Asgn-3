0e5b9cc3cd981d4cfbd5ecec7d628d4e06325ebd
Alec Odell <alec_odell@outlook.com>
2024-05-15 21:57:05 -0700

    adds -s implementation and sums number of directories and files

--- tree.c
+++ tree.c
@@ -1,11 +1,9 @@
 #include "tree.h"
 
-
-void tree(char const *path, int depth, int *spacing, int a, int s)
+void tree(char const *path, int depth, int *spacing, int a, int s, int *dirs, int *files)
 {
-    int n, numdirs;
+    int n;
     struct dirent **direntlst;
-    numdirs = 0;
 
     n = scandir(path, &direntlst, NULL, alphasort);
     if (n == -1)
@@ -23,68 +21,103 @@ void tree(char const *path, int depth, int *spacing, int a, int s)
 
         if (direntlst[j]->d_type != DT_DIR)
         {
+            *files = *files + 1;
             for (int i = 0; i < depth; i++)
             {
-                if(TestBit(spacing, i)){
+                if (TestBit(spacing, i))
+                {
                     printf("    ");
-                } else {
+                }
+                else
+                {
                     printf("|   ");
                 }
-                
-            } 
-            if (j == (n - 1) && (numdirs == 0))
+            }
+            if (s)
             {
-                printf("`-- %s\n", direntlst[j]->d_name);
+                char *newpath = (char *)malloc(sizeof(char) * (strlen(path) + strlen(direntlst[j]->d_name) + 2));
+                sprintf(newpath, "%s/%s", path, direntlst[j]->d_name);
+                struct stat *file_status = (struct stat *)malloc(sizeof(struct stat));
+                stat(newpath, file_status);
+                if (j == (n - 1))
+                {
+                    printf("`-- [% 9lld]%s\n", file_status->st_size, direntlst[j]->d_name);
+                }
+                else
+                {
+                    printf("|-- [% 9lld]%s\n", file_status->st_size, direntlst[j]->d_name);
+                }
+                free(file_status);
+                free(newpath);
             }
             else
             {
-                printf("|-- %s\n", direntlst[j]->d_name);
+                if (j == (n - 1))
+                {
+                    printf("`-- %s\n", direntlst[j]->d_name);
+                }
+                else
+                {
+                    printf("|-- %s\n", direntlst[j]->d_name);
+                }
             }
         }
-        else
+        else if (direntlst[j]->d_type == DT_DIR)
         {
-            numdirs++;
-        }
-    }
-
-    for (size_t m = 0; m < n; m++)
-    {
-        if ((strcmp(direntlst[m]->d_name, "..") == 0) || (strcmp(direntlst[m]->d_name, ".") == 0) || (*direntlst[m]->d_name == '.' && !a))
-        {
-            continue;
-        }
-
-        if (direntlst[m]->d_type == DT_DIR)
-        {
-            numdirs--;
+            *dirs = *dirs + 1;
             for (int i = 0; i < depth; i++)
             {
-                if(TestBit(spacing, i)){
+                if (TestBit(spacing, i))
+                {
                     printf("    ");
-                } else {
+                }
+                else
+                {
                     printf("|   ");
                 }
-                
-            } 
+            }
 
-            char *newpath = (char *)malloc(sizeof(char) * (strlen(path) + strlen(direntlst[m]->d_name) + 2));
-            sprintf(newpath, "%s/%s", path, direntlst[m]->d_name);
-            if (m == (n - 1) || (numdirs == 0))
+            char *newpath = (char *)malloc(sizeof(char) * (strlen(path) + strlen(direntlst[j]->d_name) + 2));
+            sprintf(newpath, "%s/%s", path, direntlst[j]->d_name);
+
+            if (s)
             {
-                printf("`-- %s\n", direntlst[m]->d_name);
-                SetBit(spacing, depth);
-                tree(newpath, depth + 1, spacing, a, s);
+                struct stat *file_status = (struct stat *)malloc(sizeof(struct stat));
+                stat(newpath, file_status);
+                if (j == (n - 1))
+                {
+                    printf("`-- [% 9lld]%s\n", file_status->st_size, direntlst[j]->d_name);
+                    SetBit(spacing, depth);
+                    tree(newpath, depth + 1, spacing, a, s, dirs, files);
+                }
+                else
+                {
+                    printf("|-- [% 9lld]%s\n", file_status->st_size, direntlst[j]->d_name);
+                    tree(newpath, depth + 1, spacing, a, s, dirs, files);
+                }
+                free(file_status);
             }
             else
             {
-                printf("|-- %s\n", direntlst[m]->d_name);
-                tree(newpath, depth + 1, spacing, a, s);
+                if (j == (n - 1))
+                {
+                    printf("`-- %s\n", direntlst[j]->d_name);
+                    SetBit(spacing, depth);
+                    tree(newpath, depth + 1, spacing, a, s, dirs, files);
+                }
+                else
+                {
+                    printf("|-- %s\n", direntlst[j]->d_name);
+                    tree(newpath, depth + 1, spacing, a, s, dirs, files);
+                }
             }
+
             free(newpath);
         }
     }
 
-    for (size_t i = 0; i < n; i++) {
+    for (size_t i = 0; i < n; i++)
+    {
         free(direntlst[i]);
     }
     free(direntlst);
@@ -94,6 +127,9 @@ int main(int argc, char const *argv[])
 {
     int a = 0;
     int s = 0;
+
+    int dirs = 1;
+    int files = 0;
 
     if (argc < 2 || argc > 4)
     {
@@ -134,17 +170,24 @@ int main(int argc, char const *argv[])
     }
 
     char *path = realpath(argv[1], NULL);
-    printf("%s\n", argv[1]);
 
-    int A[10]; //bit array
+    if(s){
+        struct stat *file_status = (struct stat *)malloc(sizeof(struct stat));
+        stat(path, file_status);
+        printf("[% 9lld]%s\n", file_status->st_size, argv[1]);
+        free(file_status);
+    } else {
+        printf("%s\n", argv[1]);
+    }
+
+    int A[10]; // bit array
     int i;
     for (i = 0; i < 10; i++)
         A[i] = 0; // Clear the bit array
 
-    tree(path, 0, A, a, s);
+    tree(path, 0, A, a, s, &dirs, &files);
 
     printf("\n");
+    printf("%d directories, %d files\n", dirs, files);
     free(path);
-
-
 }
