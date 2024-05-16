f8ac81fe7be343b5434ca3c4d0753be5321df1f2
Alec Odell <alec_odell@outlook.com>
2024-05-15 22:15:29 -0700

    fixes warnings and flag input

--- tree.c
+++ tree.c
@@ -41,11 +41,11 @@ void tree(char const *path, int depth, int *spacing, int a, int s, int *dirs, in
                 stat(newpath, file_status);
                 if (j == (n - 1))
                 {
-                    printf("`-- [% 9lld]%s\n", file_status->st_size, direntlst[j]->d_name);
+                    printf("`-- [% 9ld]%s\n", file_status->st_size, direntlst[j]->d_name);
                 }
                 else
                 {
-                    printf("|-- [% 9lld]%s\n", file_status->st_size, direntlst[j]->d_name);
+                    printf("|-- [% 9ld]%s\n", file_status->st_size, direntlst[j]->d_name);
                 }
                 free(file_status);
                 free(newpath);
@@ -86,13 +86,13 @@ void tree(char const *path, int depth, int *spacing, int a, int s, int *dirs, in
                 stat(newpath, file_status);
                 if (j == (n - 1))
                 {
-                    printf("`-- [% 9lld]%s\n", file_status->st_size, direntlst[j]->d_name);
+                    printf("`-- [% 9ld]%s\n", file_status->st_size, direntlst[j]->d_name);
                     SetBit(spacing, depth);
                     tree(newpath, depth + 1, spacing, a, s, dirs, files);
                 }
                 else
                 {
-                    printf("|-- [% 9lld]%s\n", file_status->st_size, direntlst[j]->d_name);
+                    printf("|-- [% 9ld]%s\n", file_status->st_size, direntlst[j]->d_name);
                     tree(newpath, depth + 1, spacing, a, s, dirs, files);
                 }
                 free(file_status);
@@ -128,6 +128,8 @@ int main(int argc, char const *argv[])
     int a = 0;
     int s = 0;
 
+    int pathidx = 0;
+
     int dirs = 1;
     int files = 0;
 
@@ -136,48 +138,54 @@ int main(int argc, char const *argv[])
         fprintf(stderr, "Usage: %s [-as] <file_system_directory>\n", argv[0]);
         return EXIT_FAILURE;
     }
-    if (argc > 2)
+    if (argc == 2)
     {
-        if (strcmp(argv[2], "-s") == 0)
-            s = 1;
-        else if (strcmp(argv[2], "-a") == 0)
-            a = 1;
-        else
-        {
-            fprintf(stderr, "Usage: %s [-as] <file_system_directory>\n", argv[0]);
-            return EXIT_FAILURE;
-        }
+        pathidx = 1;
     }
-    if (argc == 4)
+    else
     {
-        if (strcmp(argv[3], "-a") == 0 && s)
-            a = 1;
-        else if (strcmp(argv[3], "-s") == 0 && a)
-            s = 1;
-        else
+        for (size_t i = 1; i < argc; i++)
         {
-            fprintf(stderr, "Usage: %s [-as] <file_system_directory>\n", argv[0]);
-            return EXIT_FAILURE;
+            if (strcmp(argv[i], "-s") == 0 && s != 1)
+            {
+                s = 1;
+            }
+            else if (strcmp(argv[2], "-a") == 0 && a != 1)
+            {
+                a = 1;
+            }
+            else if (pathidx != 0)
+            {
+                fprintf(stderr, "Usage: %s [-as] <file_system_directory>\n", argv[0]);
+                return EXIT_FAILURE;
+            }
+            else
+            {
+                pathidx = i;
+            }
         }
     }
 
     struct stat buf;
-    int r = stat(argv[1], &buf);
+    int r = stat(argv[pathidx], &buf);
     if (r == -1 || !S_ISDIR(buf.st_mode))
     {
         fprintf(stderr, "Usage: %s  [-as] <file_system_directory>\n", argv[0]);
         return EXIT_FAILURE;
     }
 
-    char *path = realpath(argv[1], NULL);
+    char *path = realpath(argv[pathidx], NULL);
 
-    if(s){
+    if (s)
+    {
         struct stat *file_status = (struct stat *)malloc(sizeof(struct stat));
         stat(path, file_status);
-        printf("[% 9lld]%s\n", file_status->st_size, argv[1]);
+        printf("[% 9ld]%s\n", file_status->st_size, argv[pathidx]);
         free(file_status);
-    } else {
-        printf("%s\n", argv[1]);
+    }
+    else
+    {
+        printf("%s\n", argv[pathidx]);
     }
 
     int A[10]; // bit array
